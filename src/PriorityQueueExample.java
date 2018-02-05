15882bb7762edd90c2c1114e5e4b1c466958d68f
pypalkar23 <pypalkar23>
2018-02-05 21:24:54 +0530

    Priority Queue

---
+++ src/PriorityQueueExample.java
@@ -0,0 +1,13 @@
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+class PriorityQueueExample{
+    public static void main(String[] args) {
+        Queue<String> pQueue=new PriorityQueue<String>();
+        pQueue.add("ACB");
+        pQueue.add("ABC");
+        
+        for(String s:pQueue)
+            System.out.println(s);
+    }
+}
