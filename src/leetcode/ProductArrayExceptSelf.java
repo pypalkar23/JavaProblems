d5ee819ddd12615e4ecd3c400896c547e4d5ec47
mandar.palkar <mandar.palkar@gupshup.io>
2020-08-08 00:08:21 +0530

    Added comments to product array self

--- src/leetcode/ProductArrayExceptSelf.java
+++ src/leetcode/ProductArrayExceptSelf.java
@@ -7,12 +7,15 @@ class ProductArrayExceptSelf {
         int left = 1;
         int right = 1;
         result[0] = 1;
+        
         for (int i = 1; i < n; i++) {
+            //store the result of multiplication from 0 to i-1 lets call it 'left-multiply-result';
             left = nums[i - 1] * left;
             result[i] = left;
         }
 
         for (int i = n - 2; i >= 0; i--) {
+            //calculate the mutiplication from end to i+1 and multiply it with the 'left-multiply-result' calcuated earlier
             right = nums[i + 1] * right;
             result[i] *= right;
         }
