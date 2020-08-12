609840c2378e2d4e979bec37b0590b8affbe6865
mandar.palkar <mandar.palkar@gupshup.io>
2020-08-12 23:29:50 +0530

    Adding programs

---
+++ src/leetcode/SumofTwoIntegers.java
@@ -0,0 +1,22 @@
+package leetcode;
+
+public class SumofTwoIntegers {
+    public static void main(String[] args) {
+        SumofTwoIntegers s=new SumofTwoIntegers();
+        System.out.println(s.getSum(4,5));
+        System.out.println(s.getSum(4,-5));
+    }
+
+    /*Look at the python solution as well it need use of mask 0xffffffff here
+    https://leetcode.com/problems/sum-of-two-integers/discuss/776952/Python-BEST-LeetCode-371-Explanation-for-Python*/
+    public int getSum(int a, int b) {
+        while (b != 0) {
+            int carry = a & b;// and is like carrying
+            a = a ^ b;// xor is like summing
+            b = carry << 1;
+        }
+
+        return a;
+
+    }
+}
---
+++ src/misc/Bits.java
@@ -0,0 +1,11 @@
+package misc;
+
+public class Bits {
+    public static void main(String[] args) {
+        int m=1;
+        int n=-1;
+        System.out.println(Integer.toBinaryString(1));
+        System.out.println((Integer.toBinaryString(-1)).length());
+           
+    }
+}
