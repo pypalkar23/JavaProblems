99a65102c4eb57f9973add8700c5f84283543ac2
pypalkar23 <mandar.palkar94@gmail.com>
2020-08-29 18:42:21 +0530

    Adding new Program

--- src/misc/CharAdd.java
+++ src/misc/CharAdd.java
@@ -1,8 +1,8 @@
 package misc;
 class CharAdd{
     public static void main(String[] args) {
-        String sample="ABC";       
-        System.out.println(sample.charAt(0)+sample.charAt(1));
-       
+        String sample="123";       
+        
+        
     }
 }
