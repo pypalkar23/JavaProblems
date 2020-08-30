faf8ab4ee2f5462bb0b9ecb5a49e1ba9dddd5770
pypalkar23 <mandar.palkar94@gmail.com>
2020-08-30 13:31:07 +0530

    Adding Program

---
+++ src/leetcode/PalindromePartitioning.java
@@ -0,0 +1,48 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PalindromePartitioning {
+
+    public static void main(String[] args) {
+        List<List<String>> ans= new PalindromePartitioning().partition("aaba");
+        for (List<String> l : ans) {
+            for (String s : l)
+                System.out.print(s + " ");
+            System.out.println("");
+        }
+    }
+
+    public List<List<String>> partition(String s) {
+        List<List<String>> finalResult = new ArrayList<>();
+        List<String> intermediateCombos = new ArrayList<>();
+        findCombos(s, 0, finalResult, intermediateCombos);
+        return finalResult;
+    }
+
+    public void findCombos(String s, int start, List<List<String>> finalResult, List<String> intermediateCombos) {
+        if (start == s.length()) {
+            //that means we have traversed till the end and its safe to add intermediate combos to main list
+            finalResult.add(new ArrayList<String>(intermediateCombos));
+            return;
+        }
+
+        for (int i = start; i < s.length(); i++) {
+            if(isPalidrome(s, start, i)){
+                intermediateCombos.add(s.substring(start, i+1));
+                //find the next palindromes after current partition i
+                findCombos(s, i+1, finalResult, intermediateCombos);
+                intermediateCombos.remove(intermediateCombos.size()-1);
+            }
+        }
+    }
+
+    public boolean isPalidrome(String s, int start, int end) {
+        while (start < end) {
+            if (s.charAt(start++) != s.charAt(end--))
+                return false;
+        }
+        return true;
+    }
+}
